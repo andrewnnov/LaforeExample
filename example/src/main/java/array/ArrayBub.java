01a641f8e86f7251fa511fcf322a8d1fcff08dbe
andreybannikov <andrewnnov@yandex.ru>
2020-12-06 17:35:08 +0300

    selection sort

--- example/src/main/java/array/ArrayBub.java
+++ example/src/main/java/array/ArrayBub.java
@@ -24,19 +24,45 @@ public class ArrayBub {
 
     public void bubbleSort() {
         int out, in;
-        for (out = nElems - 1; out > 1; out--) {
+        for (out = nElems - 1; out >= 1; out--) {
             for (in = 0; in < out; in++) {
                 if(a[in] > a[in + 1]) {
                     swap(in, in + 1);
                 }
             }
         }
-
     }
 
     private void swap(int one, int two) {
         long temp = a[one];
         a[one] = a[two];
         a[two] = temp;
+    }
+
+//    public void selectionSort() {
+//        int out, in, min;
+//
+//        for (out = 0; out < nElems - 1; out++) {
+//            min = out;
+//            for (in = out + 1; in < nElems; in++) {
+//                if(a[in] < a[min]) {
+//                    min = in;
+//                }
+//            }
+//            swap(out, min);
+//        }
+//    }
+
+    public void selectSort() {
+        int out, in, min;
+        for(out = 0; out < nElems - 1; out++) {
+            min = out;
+            for (in = out + 1; in < nElems; in++) {
+                if(a[in] < a[min]) {
+                    min = in;
+                }
+            }
+            swap(out, min);
+        }
     }
 }
